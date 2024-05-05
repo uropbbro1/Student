e2a19efc5d125a6b5beba383ca21e60c968759e9
Avdey Tolstov <avdey.tolstov@saritasa.com>
2024-05-05 19:29:25 +0500

    Add sqlite database

--- tolstov/tolstov_5/Notes/Notes/Notes/App.xaml.cs
+++ tolstov/tolstov_5/Notes/Notes/Notes/App.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using Notes.Data;
 using Xamarin.Forms;
 
 
@@ -7,13 +8,26 @@ namespace Notes
 {
     public partial class App : Application
     {
-        public static string FolderPath { get; private set; }
+        static NoteDatabase database;
+
+
+        // Create the database connection as a singleton.
+        public static NoteDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new NoteDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Notes.db3"));
+                }
+                return database;
+            }
+        }
 
 
         public App()
         {
             InitializeComponent();
-            FolderPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData));
             MainPage = new AppShell();
         }
 
--- tolstov/tolstov_5/Notes/Notes/Notes/Views/NoteEntryPage.xaml.cs
+++ tolstov/tolstov_5/Notes/Notes/Notes/Views/NoteEntryPage.xaml.cs
@@ -2,7 +2,7 @@
 using System.IO;
 using Notes.Models;
 using Xamarin.Forms;
-
+using System.Threading.Tasks;
 
 namespace Notes.Views
 {
@@ -26,17 +26,13 @@ namespace Notes.Views
             BindingContext = new Note();
         }
 
-        void LoadNote(string filename)
+        async void LoadNote(string itemId)
         {
             try
             {
+                int id = Convert.ToInt32(itemId);
                 // Retrieve the note and set it as the BindingContext of the page.
-                Note note = new Note
-                {
-                    Filename = filename,
-                    Text = File.ReadAllText(filename),
-                    Date = File.GetCreationTime(filename)
-                };
+                Note note = await App.Database.GetNoteAsync(id);
                 BindingContext = note;
             }
             catch (Exception)
@@ -49,17 +45,10 @@ namespace Notes.Views
         async void OnSaveButtonClicked(object sender, EventArgs e)
         {
             var note = (Note)BindingContext;
-
-            if (string.IsNullOrWhiteSpace(note.Filename))
+            note.Date = DateTime.UtcNow;
+            if (!string.IsNullOrWhiteSpace(note.Text))
             {
-                // Save the file.
-                var filename = Path.Combine(App.FolderPath, $"{Path.GetRandomFileName()}.notes.txt");
-                File.WriteAllText(filename, note.Text);
-            }
-            else
-            {
-                // Update the file.
-                File.WriteAllText(note.Filename, note.Text);
+                await App.Database.SaveNoteAsync(note);
             }
 
 
@@ -71,13 +60,7 @@ namespace Notes.Views
         async void OnDeleteButtonClicked(object sender, EventArgs e)
         {
             var note = (Note)BindingContext;
-
-
-            // Delete the file.
-            if (File.Exists(note.Filename))
-            {
-                File.Delete(note.Filename);
-            }
+            await App.Database.DeleteNoteAsync(note);
 
 
             // Navigate backwards
